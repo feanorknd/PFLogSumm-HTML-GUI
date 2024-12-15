f54c0a93a25f5c4f88c61a90fce8aebc2d53a16b
Regino Morilla <19608497+feanorknd@users.noreply.github.com>
2024-12-15 21:22:38 +0100

    latest

--- pflogsummUIReport.sh
+++ pflogsummUIReport.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Debug option - should be disabled unless required
-set -x
+#set -x
 #=====================================================================================================================
 #   DESCRIPTION  Generating a stand alone web report for postix log files, 
 #                Runs on all Linux platforms with postfix installed
